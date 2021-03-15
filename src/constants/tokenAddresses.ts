b59821a3055638ecb7331cba86b2ef207b934a85
hashparty1 <hashparty@protonmail.com>
2021-03-15 13:51:37 -0700

    ...

--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -1,4 +1,4 @@
 export const sushi = '0x2f7682b64b88149Ba3250AEe32Db712964de5fA9'
 export const sushiv2 = '0x2f7682b64b88149Ba3250AEe32Db712964de5fA9'
 export const sushiAddress = '0x2f7682b64b88149Ba3250AEe32Db712964de5fA9'
-export const masterChefAddress = '0x3af59a5286587bdf79f423c161a06828288ca546'
+export const masterChefAddress = '0xE57369D72D25160CB16AD421B3A037D5c818728c'
